6370a4bdcafece8704f77485ab5860bb28c6d1c6
Maria Rita <100208753+projetoMariaritado@users.noreply.github.com>
2023-09-12 12:40:10 -0300

    Add files via upload

---
+++ funcoes/funcoes.js
@@ -0,0 +1,10 @@
+saldo = 1000
+function dadosacesso(){
+    numcartao = prompt("Digite o número do cartao: ")
+    senha = prompt("Digite a Senha: ")
+}
+
+function saque(valor){
+   saldo = saldo - valor
+   return saldo
+}
